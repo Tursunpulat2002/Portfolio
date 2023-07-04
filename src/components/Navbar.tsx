d51a2685e5a1467a1d716fbc74a22fa9a8565992
tursunpulat2002 <tursunpulat2002@gmail.com>
2023-07-04 01:38:06 -0400

    Added navbar and hero section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,19 @@
 import React from "react";
+import style from "../styles/navbar.module.css"
 
 function Navbar(){
     return (
-        <div>Navbar</div>
+        <div className={style.root}>
+            <div className={style.logo}>
+                <p className="primary-text">Tursunpulat Orolov</p>
+            </div>
+            <div className={style.links}>
+                <p>Home</p>
+                <p>About</p>
+                <p>Projects</p>
+                <p>Contact</p>
+            </div>
+        </div>
     )
 }
 
