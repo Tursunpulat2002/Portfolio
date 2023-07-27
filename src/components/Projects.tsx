7972288d9654b1038783c4f0cdfdafd99d85583f
tursunpulat2002 <tursunpulat2002@gmail.com>
2023-07-27 19:00:09 -0400

    Added job board app project.

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,119 +6,148 @@ import Image from "next/image";
 import { CodeBracketIcon, BoltIcon } from "@heroicons/react/24/solid";
 
 interface project {
-    image: string;
-    title: string;
-    description: string;
-    repo: string;
-    live?: string;
-    tech: Array<skill>;
+  image: string;
+  title: string;
+  description: string;
+  repo: string;
+  live?: string;
+  tech: Array<skill>;
 }
 
 const projects: Array<project> = [
-    {
-        image: "/imgs/type-master.png",
-        title: "Type Master",
-        description:
-            "It offers timed typing exercises with real-time feedback, allowing users to track their words per minute (WPM) and accuracy. With comprehensive performance statistics and progress charts, it serves as a valuable resource for individuals seeking to enhance their typing skills. With comprehensive performance statistics and progress charts, it serves as a valuable resource for individuals seeking to enhance their typing skills.",
-        repo: "https://github.com/Tursunpulat2002/type-master",
-        live: "https://tursunpulat2002.github.io/type-master/",
-        tech: [
-            {
-                image: "html-5.svg",
-                title: "HTML",
-            },
-            {
-                image: "css-3.svg",
-                title: "CSS",
-            },
-            {
-                image: "javascript.svg",
-                title: "Javascript",
-            },
-        ],
-    },
-    {
-        image: "/imgs/culinary-delights.png",
-        title: "Culinary Delights",
-        description:
-            "This is a native iOS app built with SwiftUI that allows users to browse recipes using the MealDB API. The app fetches data from the API to display a list of meals in various categories, and provides detailed information for each meal.",
-        repo: "https://github.com/Tursunpulat2002/Culinary-Delights",
-        tech: [
-            {
-                image: "swift.svg",
-                title: "Swift",
-            },
-            {
-                image: "objective-c.svg",
-                title: "Objective-C",
-            },
-        ],
-    },
-    {
-        image: "/imgs/geoguess.png",
-        title: "Geoguess",
-        description:
-            "GeoGuess is an interactive iOS app developed using Swift UI and the Google Maps API. It challenges users to test their geographical knowledge by guessing locations based on random street views.",
-        repo: "https://github.com/Tursunpulat2002/Class-Projects/tree/main/Geoguess",
-        tech: [
-            {
-                image: "swift.svg",
-                title: "Swift",
-            },
-            {
-                image: "objective-c.svg",
-                title: "Objective-C",
-            },
-        ],
-    },
+  {
+    image: "/imgs/job-list.png",
+    title: "Job Board App",
+    description:
+      "I successfully developed a React Native application, akin to a job board similar to Indeed, enabling users to access job listings along with detailed descriptions and requirements. The app seamlessly integrates with the JSearch API, providing real-time access to a wide range of job opportunities and empowering job seekers to make informed career decisions.",
+    repo: "https://github.com/Tursunpulat2002/React-Native-JobBoard-App",
+    tech: [
+      {
+        image: "html-5.svg",
+        title: "HTML",
+      },
+      {
+        image: "css-3.svg",
+        title: "CSS",
+      },
+      {
+        image: "typescript.svg",
+        title: "Typescript",
+      },
+      {
+        image: "react.svg",
+        title: "React Native",
+      },
+      {
+        image: "expo.svg",
+        title: "Expo",
+      },
+    ],
+  },
+  {
+    image: "/imgs/type-master.png",
+    title: "Type Master",
+    description:
+      "It offers timed typing exercises with real-time feedback, allowing users to track their words per minute (WPM) and accuracy. With comprehensive performance statistics and progress charts, it serves as a valuable resource for individuals seeking to enhance their typing skills. With comprehensive performance statistics and progress charts, it serves as a valuable resource for individuals seeking to enhance their typing skills.",
+    repo: "https://github.com/Tursunpulat2002/type-master",
+    live: "https://tursunpulat2002.github.io/type-master/",
+    tech: [
+      {
+        image: "html-5.svg",
+        title: "HTML",
+      },
+      {
+        image: "css-3.svg",
+        title: "CSS",
+      },
+      {
+        image: "javascript.svg",
+        title: "Javascript",
+      },
+    ],
+  },
+  {
+    image: "/imgs/culinary-delights.png",
+    title: "Culinary Delights",
+    description:
+      "This is a native iOS app built with SwiftUI that allows users to browse recipes using the MealDB API. The app fetches data from the API to display a list of meals in various categories, and provides detailed information for each meal.",
+    repo: "https://github.com/Tursunpulat2002/Culinary-Delights",
+    tech: [
+      {
+        image: "swift.svg",
+        title: "Swift",
+      },
+      {
+        image: "objective-c.svg",
+        title: "Objective-C",
+      },
+    ],
+  },
+  {
+    image: "/imgs/geoguess.png",
+    title: "Geoguess",
+    description:
+      "GeoGuess is an interactive iOS app developed using Swift UI and the Google Maps API. It challenges users to test their geographical knowledge by guessing locations based on random street views.",
+    repo: "https://github.com/Tursunpulat2002/Class-Projects/tree/main/Geoguess",
+    tech: [
+      {
+        image: "swift.svg",
+        title: "Swift",
+      },
+      {
+        image: "objective-c.svg",
+        title: "Objective-C",
+      },
+    ],
+  },
 ];
 
 function Projects() {
-    return (
-        <div className={styles.root}>
-            <h2 id="projects">Projects</h2>
-            <div className={styles.projectsContainer}>
-                {projects.map((project, i) => {
-                    return (
-                        <div className={styles.projectContainer} key={i}>
-                            <div className={styles.imageContainer}>
-                                <Image height={1000} width={1000} src={project.image} alt={project.title} />
-                            </div>
-                            <div className={styles.description}>
-                                <h2>{project.title}</h2>
-                                <p>{project.description}</p>
-                            </div>
-                            <div className={styles.skillsContainer}>
-                                {project.tech.map((skill, j) => {
-                                    return <Image height={30} width={30} src={skill.image} alt={skill.title} key={j} />;
-                                })}
-                            </div>
-                            <div className={styles.buttonsContainer}>
-                                <button
-                                    onClick={() => {
-                                        window.open(project.repo, "_blank");
-                                    }}
-                                >
-                                    <p>Repo</p>
-                                    <CodeBracketIcon className={styles.buttonIcon} />
-                                </button>
-                                {project.live ? (
-                                    <button
-                                        onClick={() => {
-                                            window.open(project.live, "_blank");
-                                        }}
-                                    >
-                                        <p>Live</p>
-                                        <BoltIcon className={styles.buttonIcon} />
-                                    </button>
-                                ) : null}
-                            </div>
-                        </div>
-                    );
+  return (
+    <div className={styles.root}>
+      <h2 id="projects">Projects</h2>
+      <div className={styles.projectsContainer}>
+        {projects.map((project, i) => {
+          return (
+            <div className={styles.projectContainer} key={i}>
+              <div className={styles.imageContainer}>
+                <Image height={1000} width={1000} src={project.image} alt={project.title} />
+              </div>
+              <div className={styles.description}>
+                <h2>{project.title}</h2>
+                <p>{project.description}</p>
+              </div>
+              <div className={styles.skillsContainer}>
+                {project.tech.map((skill, j) => {
+                  return <Image height={30} width={30} src={skill.image} alt={skill.title} key={j} />;
                 })}
+              </div>
+              <div className={styles.buttonsContainer}>
+                <button
+                  onClick={() => {
+                    window.open(project.repo, "_blank");
+                  }}
+                >
+                  <p>Repo</p>
+                  <CodeBracketIcon className={styles.buttonIcon} />
+                </button>
+                {project.live ? (
+                  <button
+                    onClick={() => {
+                      window.open(project.live, "_blank");
+                    }}
+                  >
+                    <p>Live</p>
+                    <BoltIcon className={styles.buttonIcon} />
+                  </button>
+                ) : null}
+              </div>
             </div>
-        </div>
-    );
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,74 +3,76 @@ import styles from "../styles/skills.module.css";
 import Skill, { skill } from "./Skill";
 
 interface SkillObject {
-    category: string;
-    skills: Array<skill>;
+  category: string;
+  skills: Array<skill>;
 }
 
 const skillData: Array<SkillObject> = [
-    {
-        category: "Languages",
-        skills: [
-            { image: "swift.svg", title: "Swift" },
-            { image: "objective-c.svg", title: "Objective-C" },
-            { image: "kotlin.svg", title: "Kotlin" },
-            { image: "javascript.svg", title: "Javascript" },
-            { image: "typescript.svg", title: "Typescript" },
-            { image: "java.svg", title: "Java" },
-            { image: "python.svg", title: "Python" },
-            { image: "html-5.svg", title: "HTML" },
-            { image: "css-3.svg", title: "CSS" },
-            { image: "c.svg", title: "C" },
-            { image: "c-plusplus.svg", title: "C++" },
-            { image: "c-sharp.svg", title: "C#" },
-        ],
-    },
-    {
-        category: "Frameworks & Libraries",
-        skills: [
-            { image: "uikit.svg", title: "UIKit" },
-            { image: "webkit.svg", title: "WebKit" },
-            { image: "nextjs.svg", title: "Next.js" },
-            { image: "react.svg", title: "React/React Native" },
-            { image: "bootstrap.svg", title: "Bootstrap" },
-            { image: "tailwindcss.svg", title: "Tailwind" },
-            { image: "flask.svg", title: "Flask" },
-            { image: "android.svg", title: "Android" },
-        ],
-    },
-    {
-        category: "Tools & Data",
-        skills: [
-            { image: "mysql.svg", title: "MySql" },
-            { image: "sqlite.svg", title: "Sqlite" },
-            { image: "nodejs.svg", title: "Node.js" },
-            { image: "figma.svg", title: "Figma" },
-            { image: "vercel.svg", title: "Vercel" },
-            { image: "xcode.svg", title: "XCode" },
-            { image: "cocoapods.svg", title: "Cocoapods" },
-            { image: "github.svg", title: "GitHub" },
-            { image: "git.svg", title: "Git" },
-        ],
-    },
+  {
+    category: "Languages",
+    skills: [
+      { image: "swift.svg", title: "Swift" },
+      { image: "objective-c.svg", title: "Objective-C" },
+      { image: "kotlin.svg", title: "Kotlin" },
+      { image: "javascript.svg", title: "Javascript" },
+      { image: "typescript.svg", title: "Typescript" },
+      { image: "java.svg", title: "Java" },
+      { image: "python.svg", title: "Python" },
+      { image: "html-5.svg", title: "HTML" },
+      { image: "css-3.svg", title: "CSS" },
+      { image: "c.svg", title: "C" },
+      { image: "c-plusplus.svg", title: "C++" },
+      { image: "c-sharp.svg", title: "C#" },
+    ],
+  },
+  {
+    category: "Frameworks & Libraries",
+    skills: [
+      { image: "uikit.svg", title: "UIKit" },
+      { image: "webkit.svg", title: "WebKit" },
+      { image: "nextjs.svg", title: "Next.js" },
+      { image: "react.svg", title: "ReactJS" },
+      { image: "react.svg", title: "React Native" },
+      { image: "bootstrap.svg", title: "Bootstrap" },
+      { image: "tailwindcss.svg", title: "Tailwind" },
+      { image: "flask.svg", title: "Flask" },
+      { image: "android.svg", title: "Android" },
+    ],
+  },
+  {
+    category: "Tools & Data",
+    skills: [
+      { image: "mysql.svg", title: "MySql" },
+      { image: "sqlite.svg", title: "Sqlite" },
+      { image: "nodejs.svg", title: "Node.js" },
+      { image: "figma.svg", title: "Figma" },
+      { image: "vercel.svg", title: "Vercel" },
+      { image: "expo.svg", title: "Expo" },
+      { image: "xcode.svg", title: "XCode" },
+      { image: "cocoapods.svg", title: "Cocoapods" },
+      { image: "github.svg", title: "GitHub" },
+      { image: "git.svg", title: "Git" },
+    ],
+  },
 ];
 
 function Skills() {
-    return (
-        <div className={styles.root}>
-            {skillData.map((skill, i) => {
-                return (
-                    <div className={styles.categoryContainer} key={i}>
-                        <h2>{skill.category}</h2>
-                        <div className={styles.skillsContainer}>
-                            {skill.skills.map((item, j) => {
-                                return <Skill key={j} skill={item} />;
-                            })}
-                        </div>
-                    </div>
-                );
-            })}
-        </div>
-    );
+  return (
+    <div className={styles.root}>
+      {skillData.map((skill, i) => {
+        return (
+          <div className={styles.categoryContainer} key={i}>
+            <h2>{skill.category}</h2>
+            <div className={styles.skillsContainer}>
+              {skill.skills.map((item, j) => {
+                return <Skill key={j} skill={item} />;
+              })}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Skills;
